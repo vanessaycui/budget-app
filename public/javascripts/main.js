a8d0e9c79c60b70beef6ea5b10f3bc02b7aadec6
Vanessa <vanessa.y.cui@gmail.com>
2022-12-02 09:47:38 -0500

    refactoring code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,13 @@
-/*----- constants -----*/
-/*----- app's state (variables) -----*/
+
 /*----- cached element references -----*/
 const createDashBtn = document.querySelector("#create-dash-btn")
 const createDashForm = document.querySelector("#dashboard-creation")
 const cancelBtn = document.querySelector("#form-cancel-button")
 
 /*----- event listeners -----*/
-
 createDashBtn.addEventListener('click', (event)=>{
     createDashForm.classList.toggle("hide-form")
     createDashBtn.classList.toggle("hide-form")
-
 })
 
 cancelBtn.addEventListener('click', (event)=>{
@@ -18,6 +15,4 @@ cancelBtn.addEventListener('click', (event)=>{
     createDashBtn.classList.toggle("hide-form")
  
 })
-
-/*----- functions -----*/
 
